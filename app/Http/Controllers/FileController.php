9cdfc8b5490298c30791c531de934e7a4e56afe8
Aviansh Seth <bloggerbeg@gmail.com>
2022-05-03 18:18:50 +0530

    upload files in local machine

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -9,14 +9,16 @@ class FileController extends Controller
     function postUploadFile(Request $request) {
 
         $request->validate([
-            'myfile' => 'required|mimes:pdf,png,jgep,jpg|max:500|min:480', // size:1024 in kilobyte
+            'myfile' => 'required|mimes:pdf,png,jgep,jpg,bmp,txt,docx', // size:1024 in kilobyte
         ]);
 
-        // dd($request->myfile);
+        $fileName = time() . '.' . $request->myfile->extension(); // 112215.jpg
 
-        // $fileName = time() . '.' . $request->myfile->extension();
-        // $request->myfile->move(public_path('uploads'), $fileName);
-        // $request->session()->flash('file_upload_feedback', 'File Uploaded Successfully');
-        // return redirect()->back();
+        $request->myfile->move(public_path('uploads'), $fileName);
+
+        $request->session()->flash('file_upload_feedback', 'File Uploaded Successfully');
+        
+        return redirect()->back();
+
     }
 }
